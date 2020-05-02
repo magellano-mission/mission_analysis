e3d6c0e91b81ad2272e91b956a8baa39e096e795
MarzioAgistri <43844933+MarzioAgistri@users.noreply.github.com>
2020-05-02 22:08:38 +0200

    some roba varia boh

--- COVERAGE_STUDY/coverage_variations.m
+++ COVERAGE_STUDY/coverage_variations.m
@@ -17,8 +17,9 @@ set(0,'defaultAxesTickLabelInterpreter','latex');
 run('data.m')
 
 %% Sim
+tic
 [Min_cov_lat, time_map, N_min, N_mean] = NestedFor(wlk_vec, inclinations, bw, semi_major_axes, lon, lat, timesteps, N_orbits, alt, para);
 save('matriciozza.mat', 'Min_cov_lat')
-
+toc
 %% plot
 sim_plots(SimType, wlk_vec, inclinations, bw, semi_major_axes, Min_cov_lat);
