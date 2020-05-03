5af6cffd3118e5c2081cf4afb39004912bed1049
Filippo <filippo.inno1994@gmail.com>
2020-05-03 12:17:20 +0200

    armageddon

--- COVERAGE_STUDY/misc/check_errors.m
+++
@@ -1,6 +0,0 @@
-function check_errors(SimType, wlk_vec, inclinations, bw, semi_major_axes)
-
-
-if SimType == "FPCP" && (length(wlk_vec) ~= 1 || length(inclinations) ~= 1 || length(bw) ~= 1 || length(semi_major_axes) ~= 1)
-    error('SimType FPCP not compatible with non-scalar parameters, check data.m');
-end
